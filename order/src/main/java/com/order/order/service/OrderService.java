c07a8abac18bc65a5656bf2c94c6ec6ace357ab6
SamarakoonRMSK <rmsksamarakoon@gmail.com>
2025-06-24 10:54:15 +0530

    feat: add connection between order and inventory

--- order/src/main/java/com/order/order/service/OrderService.java
+++ order/src/main/java/com/order/order/service/OrderService.java
@@ -1,19 +1,28 @@
 package com.order.order.service;
 
+import com.inventory.inventory.dto.InventoryDTO;
+import com.order.order.common.ErrorOrderResponse;
+import com.order.order.common.OrderResponse;
+import com.order.order.common.SuccessOrderResponse;
 import com.order.order.dto.OrderDTO;
 import com.order.order.model.Orders;
 import com.order.order.repo.OrderRepo;
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 
 @Service
 @Transactional
+@RequiredArgsConstructor
 public class OrderService {
+    private final WebClient webClient;
+
     @Autowired
     private OrderRepo orderRepo;
 
@@ -25,9 +34,30 @@ public class OrderService {
         return modelMapper.map(orderList, new TypeToken<List<OrderDTO>>(){}.getType());
     }
 
-    public OrderDTO saveOrder(OrderDTO OrderDTO) {
-        orderRepo.save(modelMapper.map(OrderDTO, Orders.class));
-        return OrderDTO;
+    public OrderResponse saveOrder(OrderDTO OrderDTO) {
+
+        Integer itemId = OrderDTO.getItemId();
+
+        try {
+            InventoryDTO inventoryResponse = webClient.get()
+                    .uri(uriBuilder -> uriBuilder.path("/api/v1/item/{itemId}").build(itemId))
+                    .retrieve()
+                    .bodyToMono(InventoryDTO.class)
+                    .block();
+
+            System.out.println(inventoryResponse);
+//            Integer ProductId = inventoryResponse.getProductId();
+            if(inventoryResponse.getQuantity() > 0){
+                orderRepo.save(modelMapper.map(OrderDTO, Orders.class));
+                return new SuccessOrderResponse(OrderDTO);
+            }else{
+                return new ErrorOrderResponse("Item not available, please try later");
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 
     public OrderDTO updateOrder(OrderDTO OrderDTO) {
